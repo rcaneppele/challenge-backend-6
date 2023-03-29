f62b67d4fda3d3ee0494b1d4a900eb3c16e53525
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-03-29 15:45:14 -0300

    criacao do projeto com configuracoes do banco de dados

---
+++ src/main/java/br/com/alura/challenge/adopet/AdopetApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.challenge.adopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/challenge/adopet/AdopetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.challenge.adopet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdopetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
