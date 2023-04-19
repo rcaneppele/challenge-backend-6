f04125fe98913b311039c371865e3bed5d549a48
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-04-19 16:30:28 -0300

    devolvendo json no tratador de exceptions

--- src/main/java/br/com/alura/challenge/adopet/infra/exception/TratadorDeExceptionHandler.java
+++ src/main/java/br/com/alura/challenge/adopet/infra/exception/TratadorDeExceptionHandler.java
@@ -4,12 +4,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class TratadorDeExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity tratarErroDeValidacao(IllegalArgumentException ex) {
-        return ResponseEntity.badRequest().body(ex.getMessage());
+        return ResponseEntity.badRequest().body(new DadosErro400(LocalDateTime.now(), ex.getMessage()));
     }
+
+    private record DadosErro400(LocalDateTime timestamp, String message) {}
 
 }
